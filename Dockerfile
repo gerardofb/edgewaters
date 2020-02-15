e3a6499e04d04e2a92d56e693c8986df5e8a4e48
gerardofb <gerardo.v.flores@gmail.com>
2020-02-14 21:14:09 -0600

    host de redis para despliegue

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:10.19.0-stretch
+MAINTAINER gerardo.v.flores@gmail.com
+RUN git clone https://github.com/gerardofb/edgewaters.git
+WORKDIR edgewaters
+RUN git checkout colasmensajes
+RUN npm install
+EXPOSE 4000
--- controlador.js
+++ controlador.js
@@ -1,6 +1,6 @@
 // funciones de manejo de datos
 var saveRedis = require("redis");
-var clienteControlador = saveRedis.createClient();
+var clienteControlador = saveRedis.createClient(6379, 'redis');
 exports.addUser = function addUser(user, name){
     clienteControlador.multi()
     .hset('user:' + user, 'name', name)
