c70e266c516c7233250d05d5fca30b9778a93a60
gerardofb <gerardo.v.flores@gmail.com>
2020-02-21 16:08:39 -0600

    headers de Acess-Control-Allow-Origin

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.use("/", express.static(path.join(__dirname, '/')));
 var server = http.listen(4000, function(){
     console.log("servidor escuchando en el puerto 4000");
 });
+app.use((req, res, next) => {
+    const origin = req.get('origin');
+  
+    // TODO Add origin validation
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    next();
+  });
 const io_s = require('socket.io')(server);
 const cliente = rediscliente.createClient();
 // funciones de registro y logueo
