406b83ed653ef3d8f8b5885ba75248470eb9970d
gerardofb <gerardo.v.flores@gmail.com>
2020-02-21 16:34:26 -0600

    se reemplazÃó host de redis

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM node:10.19.0-stretch
 MAINTAINER gerardo.v.flores@gmail.com
 RUN git clone https://github.com/gerardofb/edgewaters.git
 WORKDIR edgewaters
-RUN git checkout colasmensajes
+RUN git fetch
+RUN git checkout contenedor_devready
 RUN git pull
 RUN npm install
 EXPOSE 4000
--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
     next();
   });
 const io_s = require('socket.io')(server);
-const cliente = rediscliente.createClient();
+const cliente = rediscliente.createClient(6379,"redis");
 // funciones de registro y logueo
 function registerProcess(req, res){
     console.log('intentando registrar usuario');
@@ -84,7 +84,7 @@ app.get('/loginprocess', cors(), loginProcess);
 cliente.subscribe("edgewaters");
 
 io_s.adapter(redis({
-    host: '127.0.0.1',
+    host: 'redis',
     port: 6379,
     subClient:cliente,
 }));
